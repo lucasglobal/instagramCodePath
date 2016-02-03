09cd6ef62ac2683869d0d7b5366e7e8f3b36e23d
Lucas Andrade <passaroalado@hotmail.com>
2016-02-02 21:54:10 -0600

    request data from instagram api

--- instagram/instagram/PhotosViewController.swift
+++ instagram/instagram/PhotosViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import AFNetworking
 
 class PhotosViewController: UIViewController {
 
+    var dataFromRequest: NSDictionary!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            self.dataFromRequest = responseDictionary
+                            print("response: \(responseDictionary)")
+                    }
+                }
+        });
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
